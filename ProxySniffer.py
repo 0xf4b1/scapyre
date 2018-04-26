bc5da4b481bc7b372dd4f2c231446948b000bb9f
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2018-04-26 16:58:26 +0200

    Callback without return value for ProxySniffer to get rid of the print of boolean values for each received packet

--- ProxySniffer.py
+++ ProxySniffer.py
@@ -9,7 +9,10 @@ class ProxySniffer:
     def start(self):
         """ Starts the proxy to intercept incoming packets """
         print("Using sniffer")
-        sniff(iface=self.instance.iface, prn=self.instance.callback)
+        sniff(iface=self.instance.iface, prn=self.callback_sniffer)
+
+    def callback_sniffer(self, pkt):
+        self.instance.callback(pkt)
 
     def stop(self):
         pass
